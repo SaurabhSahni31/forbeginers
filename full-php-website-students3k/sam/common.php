3518b4b41bae4c0aa47be9314bfb95b60cde21d6
msgod38 <saurabhsahni31@gmail.com>
2018-10-10 23:51:08 +0530

    testing

--- full-php-website-students3k/sam/common.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("header.php");
-include("latestpost.php");
-include("topmenu.php");
-include("leftmenu.php");
-include("footermenu.php");
-?>
--- full-php-website-students3k/sam/data_db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$userdataname="root";
-$userdatapass="1234";
-$user_data="forum";
-?>
